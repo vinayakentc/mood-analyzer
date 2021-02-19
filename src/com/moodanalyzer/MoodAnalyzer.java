b860466aed8deb0ba5bfd7e6e94a88828f2aa8e4
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-19 18:58:52 +0530

    mood analyzer

---
+++ src/com/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,5 @@
+package com.moodanalyzer;
+
+public class MoodAnalyzer {
+
+}
---
+++ src/com/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,14 @@
+package com.moodanalyzer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoodAnalyzerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
